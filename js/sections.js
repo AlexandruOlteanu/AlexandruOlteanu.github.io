3566598966c97045424fc3c7fca4ece21617634a
AlexandruOlteanu <alexandruolteanu2001@gmail.com>
2023-03-17 18:30:13 +0200

    modified:   FAQ.html 	modified:   Pages/team.html 	modified:   Pages/testimonial.html 	modified:   Sections/footer-section.html 	modified:   Sections/navbar-section.html 	new file:   Sections/testimonial-section.html 	modified:   Services/SEO-ecommerce.html 	modified:   Services/business-plans.html 	modified:   Services/business-slogans.html 	modified:   Services/copywriting.html 	modified:   Services/facebook-ads.html 	modified:   Services/google-ads.html 	modified:   Services/instagram-growth.html 	modified:   Services/logo-design.html 	modified:   Services/website-development.html 	modified:   Useful Links/Privacy.html 	modified:   Useful Links/Return & Refund.html 	modified:   Useful Links/Term & Conditions.html 	modified:   about.html 	modified:   contact.html 	modified:   css/style.css 	new file:   home.html 	deleted:    index.html 	modified:   js/main.js 	modified:   js/sections.js

--- js/sections.js
+++ js/sections.js
@@ -1,8 +1,65 @@
-$(document).ready(function() {
-    $("#topbar-section").load("/Sections/topbar-section.html");
-    $("#navbar-section").load("/Sections/navbar-section.html");
-    $("#contact-section").load("/Sections/contact-section.html");
-    $("#vendors-section").load("/Sections/vendors-section.html");
-    $("#footer-section").load("/Sections/footer-section.html");
+fetch("/Sections/topbar-section.html")
+    .then(response => {
+        return response.text();
+    })
+    .then(data => {
+        let query = document.querySelector("topbar-section");
+        if (query != null) {
+            document.querySelector("topbar-section").innerHTML = data;
+        }
 });
 
+fetch("/Sections/navbar-section.html")
+    .then(response => {
+        return response.text();
+    })
+    .then(data => {
+        let query = document.querySelector("navbar-section");
+        if (query != null) {
+            document.querySelector("navbar-section").innerHTML = data;
+        }
+});
+
+fetch("/Sections/testimonial-section.html")
+    .then(response => {
+        return response.text();
+    })
+    .then(data => {
+        let query = document.querySelector("testimonial-section");
+        if (query != null) {
+            document.querySelector("testimonial-section").innerHTML = data;
+        }
+});
+
+fetch("/Sections/contact-section.html")
+    .then(response => {
+        return response.text();
+    })
+    .then(data => {
+        let query = document.querySelector("contact-section");
+        if (query != null) {
+            document.querySelector("contact-section").innerHTML = data;
+        }
+});
+
+fetch("/Sections/vendors-section.html")
+    .then(response => {
+        return response.text();
+    })
+    .then(data => {
+        let query = document.querySelector("vendors-section");
+        if (query != null) {
+            document.querySelector("vendors-section").innerHTML = data;
+        }
+});
+
+fetch("/Sections/footer-section.html")
+    .then(response => {
+        return response.text();
+    })
+    .then(data => {
+        let query = document.querySelector("footer-section");
+        if (query != null) {
+            document.querySelector("footer-section").innerHTML = data;
+        }
+});
